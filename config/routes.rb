5e5c6576b156e2874cdb494b695efa5ce394f6e1
fatemanazari <fatemanazari78@gmail.com>
2024-01-10 11:30:34 +0330

    Create connection between home page and categories page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :transactions
   resources :categories
-  devise_for :users
+  devise_for :users, controllers: {
+  registrations: 'users/registrations',
+  sessions: 'users/sessions'
+}
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
