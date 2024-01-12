28d6f732e907417e6e69c5aaf63673d520e0bb7c
fatemanazari <fatemanazari78@gmail.com>
2024-01-12 15:39:26 +0330

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :groups, foreign_key: 'author_id'
   has_many :entities, foreign_key: 'author_id'
 
-  has_one_attached :avatar  # Assuming you want each user to have one avatar
+  has_one_attached :avatar # Assuming you want each user to have one avatar
 
   validates :name, presence: true
 end
